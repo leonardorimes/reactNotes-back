c31a893208fac13a46c59ecffb879a07e48a75ee
Leonardo <rimesleo@gmail.com>
2023-04-26 21:19:35 -0300

    adicionando teste que checa se encontra erro de email no cadastro

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,23 +1,49 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError");
+
+describe("UserCreateService", () =>{
+    
+    it("user should be create", async () => {
+        const user = {
+            name: "User Test",
+            email: "user@test.com",
+            password: "123"
+        };
+    
+    
+        const userRepositoryInMemory = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepositoryInMemory);
+        const userCreated = await userCreateService.execute(user);
+    
+    
+        expect(userCreated).toHaveProperty("id");
+    
+    
+    
+    
+    });
 
 
-it("user should be create", async () => {
-    const user = {
-        name: "User Test",
-        email: "user@test.com",
-        password: "123"
-    };
+    it("user not should be create with exists email ", async () =>{
+        const user1 = {
+            name: "User Test 1",
+            email: "user@test.com",
+            password: "123"
+        };
+  
 
+        const user2 = {
+            name: "User Test 2",
+            email: "user@test.com",
+            password: "456"
+        };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
-    const userCreated = await userCreateService.execute(user);
+        const userRepository = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepository);
 
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."));
 
-    expect(userCreated).toHaveProperty("id");
-
-
-
-
-});
+    });
+})
